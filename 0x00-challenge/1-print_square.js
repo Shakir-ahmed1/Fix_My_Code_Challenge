93f2d5526276ee4d92dba833eed54db52020d04c
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-05-28 23:51:00 +0000

    cleaning unnecessary code

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -13,13 +13,11 @@ if (process.argv.length <= 2) {
     process.stderr.write("Example: ./1-print_square.js 8\n");
     process.exit(1)
 }
-if (process.argv.length == 3) {
-	size = parseInt(process.argv[2], 10)
+size = parseInt(process.argv[2], 10)
 
-	for (let i = 0 ; i < size ; i ++) {
-	    for (let j = 0 ; j < size ; j ++) {
-		process.stdout.write("#");
-	    }
-	    process.stdout.write("\n");
-	}
+for (let i = 0 ; i < size ; i ++) {
+    for (let j = 0 ; j < size ; j ++) {
+	process.stdout.write("#");
+    }
+    process.stdout.write("\n");
 }
